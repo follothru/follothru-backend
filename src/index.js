0a31e4ffa02a92c275209dd7f02b3790788bf9a4
Andy Su <f2280c@gmail.com>
2019-01-16 18:05:12 -0500

    added builder for course

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const CourseModel = require('./models/course.model.js');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    const courseBuilder = CourseModel.CourseModel.getBuilder().buildId(22);
+    const course = courseBuilder.build();
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
--- src/models/course.model.js
+++ src/models/course.model.js
@@ -1,12 +1,53 @@
 const { BaseModel } = require('./base.model.js');
 
 class CourseModel extends BaseModel {
-  constructor() {
+  
+  constructor(builder) {
     super();
+    this._id = builder._id;
+    this._name = builder._name;
+    this._description = builder._description;
+  }
+  
+  get id(){
+    return this._id;
+  }
+  
+  get name(){
+    return this._name;
+  }
+  
+  get description(){
+    return this._description;
+  }
+  
+  // builder
+  static getBuilder(){
+    return new CourseModelBuilder();
+  }
+}
 
-    this.id;
-    this.name;
-    this.description;
+// builder
+class CourseModelBuilder{
+  constructor(){
+    this._id;
+    this._name = '';
+    this._description = '';
+  }
+  buildId(id){
+    this._id = id;
+    return this;
+  }
+  buildName(name){
+    this._name = name;
+    return this;
+  }
+  buildDescription(description){
+    this._description = description;
+    return this;
+  }
+  build(){
+    return new CourseModel(this);
   }
 }
 
