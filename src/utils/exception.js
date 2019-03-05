2f064398b67da4489d07af6d96d512268a49aac8
Chris Xu <gzchrisxjh@gmail.com>
2019-03-05 17:36:56 -0500

    Remove extra files

--- src/utils/exception.js
+++
@@ -1,8 +0,0 @@
-class Exception {
-  constructor(messageKey) {
-    this.label = messageKey._label;
-    this.message = messageKey._msg;
-  }
-}
-
-module.exports = Exception;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,5 @@
-const Exception = require('./exception.js');
-
-const MessageKey = require('./messagekey.js');
-
 const ValidationUtil = require('./validation.util.js');
 
 module.exports = {
-  Exception,
-  MessageKey,
   ValidationUtil
 };
