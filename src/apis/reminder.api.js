94e35504cd4b70908672359ee9ce4d290288d846
Andy Su <f2280c@gmail.com>
2019-03-05 12:22:23 -0500

    updated reminder api

--- src/apis/reminder.api.js
+++ src/apis/reminder.api.js
@@ -1,12 +1,6 @@
 module.exports = (() => {
   const express = require('express');
-  const {
-    ReminderService,
-    SessionService,
-    EventService,
-    ActivityService,
-    VaultService
-  } = require('../services');
+  const { SessionService } = require('../services');
 
   const router = express.Router();
 
@@ -14,14 +8,7 @@ module.exports = (() => {
     '/:reminderId',
     SessionService.authenticateSession,
     (req, res) => {
-      const { reminderId } = req.params;
-      VaultService.deleteReminderById(reminderId)
-        .then(result => {
-          res.send(result);
-        })
-        .catch(err => {
-          res.send(err);
-        });
+      //todo
     }
   );
 
