3f7721d76b076619ac3a984f0fa796df296c8dad
Chris Xu <gzchrisxjh@gmail.com>
2019-09-04 09:19:38 -0400

    Add dbUrls for different environments

--- src/js/config.js
+++ src/js/config.js
@@ -1,15 +1,18 @@
+import urls from '../env';
 require('dotenv').config();
+
+const dbUsername = process.env.DB_USERNAME;
 
 const dbPassword = process.env.DB_PASSWORD;
 
 export const port = process.env.PORT;
 
-export const nodeEnv = process.env.NODE_ENV;
+export const env = process.env.NODE_ENV || 'dev';
 
 export const devMongoDatabaseUrl = 'mongodb://mongo:27017/follothru';
 
-export const mongoDatabaseUrl = nodeEnv === 'prod' ?
-  `mongodb+srv://follothru:${encodeURI(dbPassword)}@follothru-db-rsgd2.mongodb.net/test?retryWrites=true&w=majority`
-  : devMongoDatabaseUrl;
+export const mongoDatabaseUrl = urls[env].dbUrl
+  .replace('<username>', encodeURI(dbUsername))
+  .replace('<password>', encodeURI(dbPassword));
 
 export const secretKey = process.env.SECRET_KEY;
